f87c215050498ef8d3cd0bdf55601c86a06120ab
Damian <damianezetiel1@gmail.com>
2019-11-01 10:38:41 -0300

    login design

--- vue/src/views/home/home.vue
+++ vue/src/views/home/home.vue
@@ -1,7 +1,38 @@
 <template transition="slide-x-transition">
   <div id="homePage">
     <div class="login-box">
-      
+      <h4 class="font-title">Acceda para utilizar CyberManager</h4>
+
+      <div class="login-fields">
+        <v-text-field v-model="signInData.username" class="field" label="Usuario"></v-text-field>
+        <v-text-field v-model="signInData.password" class="field" label="Contraseña"></v-text-field>
+      </div>
+      <p class="font-error">{{ signInErrors }}</p>
+      <div class="login-footer">
+        <v-btn @click="login()" class="button">Acceder</v-btn>
+      </div>
+
+      <!-- register dialog -->
+      <v-dialog class="register-dialog" v-model="dialogs.signUp" width="500">
+        <v-card>
+          <v-card-title>Cree una cuenta para iniciar en Organize</v-card-title>
+          <v-card-text>
+            <div class="login-fields">
+              <v-text-field v-model="signUpData.email" class="field" label="email"></v-text-field>
+              <v-text-field v-model="signUpData.username" class="field" label="username"></v-text-field>
+              <v-text-field v-model="signUpData.password" class="field" label="password"></v-text-field>
+            </div>
+            <p class="font-error">{{ signUpErrors }}</p>
+            <div class="login-footer">
+              <v-btn @click="register()" class="button">Crear cuenta</v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
+
+
+
     </div>
   </div>
 </template>
--- vue/src/views/home/homeCode.ts
+++ vue/src/views/home/homeCode.ts
@@ -4,4 +4,51 @@ import PageData from '../../data/PageData';
 
 export default class HomeCode extends XComponent {
     private data: any = new PageData();
+
+    private signInData: any = {
+        email: '',
+        password: '',
+      }
+      private signInErrors = '';
+    
+      private signUpData: any = {
+        username: '',
+        email: '',
+        password: ''
+      }
+      private signUpErrors = '';
+    
+      private dialogs: any = {
+        signIn: false,
+        signUp: false
+      }
+    
+      async register() {
+        // check success input data in register dialog
+        // const result: any = await this.backend.send('post:signUp', { user: this.signUpData })
+        // if (result.statusCode == 200) {
+        //   this.dialogs.signUp = false;
+        //   const userData = {
+        //     id: result.value.id,
+        //     token: result.value.token,
+        //     username: this.signUpData.username,
+        //   }
+        //   this.$store.commit('userInfo', userData)
+        //   this["$router"].push('/Events');
+        // } else {
+        //   this.signUpErrors = 'No debe dejar los campos vacios'
+        // }
+      }
+    
+      async login() {
+        // check success input data in login dialog
+        // const result: any = await this.backend.send('post:signIn', { user: this.signInData });
+        // const userData = {
+        //   id: result.value.id,
+        //   token: result.value.token,
+        //   username: result.value.username,
+        // }
+        // this.$store.commit('userInfo', userData);
+        // this["$router"].push('/Events');
+      }
 }
