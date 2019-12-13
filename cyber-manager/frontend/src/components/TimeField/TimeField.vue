74ce39782289ab90d9814f5c7e6b47a849f5b29e
Damian <damianezetiel1@gmail.com>
2019-12-13 20:15:34 -0300

    fixes in TimeField component

--- cyber-manager/frontend/src/components/TimeField/TimeField.vue
+++ cyber-manager/frontend/src/components/TimeField/TimeField.vue
@@ -22,12 +22,11 @@
       <v-time-picker v-model="time" full-width scrollable :locale="lang">
         <v-spacer></v-spacer>
         <v-btn text color="primary" @click="modal2 = false">Cancel</v-btn>
-        <v-btn text color="primary" @click="$refs.dialogHour.save(time)"
-          >OK</v-btn
-        >
+        <v-btn text color="primary" @click="$refs.dialogHour.save(time)">OK</v-btn>
       </v-time-picker>
     </v-dialog>
 
+    <!-- Date Dialog  -->
     <v-dialog
       v-else
       ref="dialogDate"
@@ -37,48 +36,80 @@
       width="290px"
     >
       <template v-slot:activator="{ on }">
-        <v-text-field
-          v-model="time"
-          :error="error"
-          :error-messages="errorMessage"
-          :label="label"
-          :prepend-icon="icon"
-          readonly
-          v-on="on"
-        ></v-text-field>
+        <div class="field-date__box">
+          <v-text-field
+            v-model="simpleDate"
+            :error="error"
+            :error-messages="errorMessage"
+            :label="label"
+            :prepend-icon="icon"
+            readonly
+            v-on="on"
+          ></v-text-field>
+
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                @click="setTodayDate()"
+                text
+                color="green"
+                v-on="on"
+                class="btn-date"
+                fab
+                small
+              >
+                <v-icon>calendar_today</v-icon>
+              </v-btn>
+            </template>
+            <span>pegar fecha actual</span>
+          </v-tooltip>
+        </div>
       </template>
+
       <v-date-picker v-model="time" scrollable :locale="lang">
         <v-spacer></v-spacer>
         <v-btn text color="primary" @click="modal2 = false">Cancel</v-btn>
-        <v-btn text color="primary" @click="$refs.dialogDate.save(time)"
-          >OK</v-btn
-        >
+        <v-btn text color="primary" @click="$refs.dialogDate.save(time)">OK</v-btn>
       </v-date-picker>
     </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
-import { Prop, Watch, Component } from 'vue-property-decorator';
-import TimeFieldCode from './TimeFieldCode';
-import './TimeFieldStyle.scss';
-import Datetime from '../../../../backend/src/utils/DateTime';
+import { Prop, Watch, Component } from "vue-property-decorator";
+import TimeFieldCode from "./TimeFieldCode";
+import "./TimeFieldStyle.scss";
 
 @Component({})
 export default class TimeField extends TimeFieldCode {
-  @Prop({ default: new Datetime().now() }) value: string;
+  @Prop({ default: new Date().toLocaleDateString() }) value: string;
+  @Prop({ default: "date" }) type!: string;
+  @Prop({ default: "es" }) lang!: string;
+  @Prop({ default: "" }) label!: string;
+  @Prop({ default: "" }) icon!: string;
+  @Prop({ default: "" }) error!: string;
+  @Prop({ default: "" }) errorMessage!: string;
 
-  @Prop({ default: 'date' }) type!: string;
-  @Prop({ default: 'es' }) lang!: string;
-  @Prop({ default: '' }) label!: string;
-  @Prop({ default: '' }) icon!: string;
-  @Prop({ default: '' }) error!: string;
-  @Prop({ default: '' }) errorMessage!: string;
-
-  @Watch('value')
+  @Watch("time")
   updateTime() {
+    this.simpleDate = this.getDate(this.time);
+    this.$emit("input", this.time);
+  }
+
+  @Watch("value")
+  updateValue() {
+    this.simpleDate = this.getDate(this.time);
     this.time = this.value;
-    this.$emit('input', this.time);
   }
 }
 </script>
+<style lang="scss">
+.field-date__box {
+  display: flex;
+
+  .btn-date {
+    position: relative;
+    margin-top: 20px;
+  }
+}
+</style>
