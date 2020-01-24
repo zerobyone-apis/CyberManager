c5baa0d30c1bba86eebe6549cd687a538b351978
Damian <damianezetiel1@gmail.com>
2020-01-24 01:00:39 -0300

    remove class only for test

--- cyber-manager/frontend/src/main.ts
+++ cyber-manager/frontend/src/main.ts
@@ -5,12 +5,12 @@ import App from "./app/app.vue";
 import router from "./router";
 import { store } from "./store";
 import 'vue-croppa/dist/vue-croppa.css';
-import vueprint from 'vue-print-nb';
+// import vueprint from 'vue-print-nb';
 
 Vue.config.productionTip = false;
 Vue.use(vuetify);
 Vue.use(Vuetify);
-Vue.use(vueprint)
+// Vue.use(vueprint)
 
 new Vue({
   vuetify,
