ca72ef49ab503e23f2c51285aa1b68f70144e90d
Damian <damianezetiel1@gmail.com>
2020-01-25 02:57:01 -0300

    add const routes

--- cyber-manager/frontend/src/types/Routes.type.ts
+++ cyber-manager/frontend/src/types/Routes.type.ts
@@ -1,5 +1,7 @@
 // routes
 export const USER_ROUTE: string = '/user';
+export const USER_SIGN_IN_ROUTE: string = `${USER_ROUTE}/signin`;
+
 export const ORDER_ROUTE: string = '/order';
 export const ENTERPRISE_ROUTE: string = '/enterprise';
 export const REPARIR_ROUTE: string = ORDER_ROUTE+'/'+'repair';
@@ -8,7 +10,3 @@ export const GET_ENDPOIT: string = 'get';
 export const POST_ENDPOIT: string = 'post';
 export const PUT_ENDPOIT: string = 'put';
 export const DELETE_ENDPOIT: string = 'delete';
-
-
-
-
