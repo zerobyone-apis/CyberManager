ecd254f3adf465613c8fae7499aed26c551741cf
ZoroIsMyMain <max3.1994@gmail.com>
2020-02-02 04:51:51 -0300

    another fix to fix the fix

--- electron/index.js
+++ electron/index.js
@@ -27,84 +27,105 @@ function createWindow(callback) {
     app.quit(); // quit again
   }
 
-  loadHomePage();
+  // The server url and process
+  let serverProcess;
+  let baseUrl;
 
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    if (callback) callback();
+  function createWindow(callback) {
+    mainWindow = new BrowserWindow({
+      width: 1200,
+      height: 950,
+      show: false, // hide until ready-to-show
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true
+      }
+    });
+
+    function stopServer() {
+      logger.info('Stopping server...');
+      app.quit(); // quit again
+    }
+
+    loadHomePage();
+
+    mainWindow.once('ready-to-show', () => {
+      mainWindow.show();
+      if (callback) callback();
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+    });
+  }
+
+  function loadHomePage() {
+    logger.info(`logHomePage: Loading home page at ${baseUrl}`);
+    // check server health and switch to main page
+
+    mainWindow.loadURL(`${baseUrl}?_=${Date.now()}`);
+    checkCount = 0;
+  }
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', function() {
+    baseUrl = `http://localhost:9000`;
+    const iconName =
+      '/home/zoroismymain/Escritorio/test/CyberManager/electron/build/system.ico';
+    const iconPath = path.join(__dirname, iconName);
+    console.log('');
+    console.log('══════════════════════════');
+    console.log('');
+    console.log(' ╦══╦      ╦═╦   ╦══╦');
+    console.log(' ╠═╝╠═╦═╦═╗║░╩╦╦╗║╔╗╠═╦═╗ ');
+    console.log(' ║╔═╣╩╣╠╣║║║░░║║║║╚╝║║║╩╣ ');
+    console.log(' ╚══╩═╩╝╚═╝╚══╬╗║╚══╩╩╩═╝ ');
+    console.log('              ╩═╩');
+
+    console.log(` CyberManager Electron is ready `);
+    console.log('');
+    console.log('══════════════════════════');
+    console.log('');
+    createWindow();
   });
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+  //Solo MacOS
+  app.on('activate', function() {
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });
+
+  app.on('will-quit', e => {
+    if (!isDev && baseUrl != null) {
+      stopServer();
+      e.preventDefault(); // will quite later after stopped the server
+    }
   });
 }
-
-function loadHomePage() {
-  logger.info(`logHomePage: Loading home page at ${baseUrl}`);
-  // check server health and switch to main page
-
-  mainWindow.loadURL(`${baseUrl}?_=${Date.now()}`);
-  checkCount = 0;
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', function() {
-  baseUrl = `http://localhost:9000`;
-  const iconName =
-    '/home/zoroismymain/Escritorio/test/CyberManager/electron/build/system.ico';
-  const iconPath = path.join(__dirname, iconName);
-  console.log('');
-  console.log('══════════════════════════');
-  console.log('');
-  console.log(' ╦══╦      ╦═╦   ╦══╦');
-  console.log(' ╠═╝╠═╦═╦═╗║░╩╦╦╗║╔╗╠═╦═╗ ');
-  console.log(' ║╔═╣╩╣╠╣║║║░░║║║║╚╝║║║╩╣ ');
-  console.log(' ╚══╩═╩╝╚═╝╚══╬╗║╚══╩╩╩═╝ ');
-  console.log('              ╩═╩');
-
-  console.log(` CyberManager Electron is ready `);
-  console.log('');
-  console.log('══════════════════════════');
-  console.log('');
-  createWindow();
-});
-
-//Solo MacOS
-app.on('activate', function() {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-app.on('will-quit', e => {
-  if (!isDev && baseUrl != null) {
-    stopServer();
-    e.preventDefault(); // will quite later after stopped the server
-  }
-});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
@@ -119,4 +140,4 @@ app.on('will-quit', e => {
 //  win.loadURL(url)
 //}
 //// Asociar al evento 'ready'
-//app.on('ready', createWindow)
+//app.on('ready', createWindows)
