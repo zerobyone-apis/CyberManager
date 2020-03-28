e9b573f05cb09554fbacb85df289a38ed5bc7220
Damian <damianezetiel1@gmail.com>
2020-03-27 22:22:55 -0300

    refactor input pdf

--- cyber-manager/frontend/src/utils/pdfDocuments/InputPDF.ts
+++ cyber-manager/frontend/src/utils/pdfDocuments/InputPDF.ts
@@ -15,60 +15,106 @@ export interface IInputPdf {
 export default class InputPdf extends Styles {
 
   generateDoc(enterprise: IEnterprise, order: IOrder) {
-    let doc: any  = new jsPDF('p', 'px', [this.pageSize.width, this.pageSize.heigth]);
+    let doc: any = new jsPDF('p', 'px', [this.pageSize.width, this.pageSize.heigth]);
 
+    let marginTop: number = -15;
     for (let i = 0; i < 2; i++) {
 
       doc.autoTable({
         didDrawCell: (data: any) => {
-          if (data.section === 'head' && data.column.index === 1) {
-
+          if (data.section === 'head' && data.column.index === 2) {
             if (enterprise.urllogo) {
               try {
                 let base64 = this.getBase64Image(document.getElementById('imageid'));
-                doc.addImage(base64, 'JPEG', data.cell.x + 130, data.cell.y - 20, 40, 40)
+                doc.addImage(base64, 'JPEG', data.cell.x, data.cell.y + 10, 80, 80)
               } catch (error) {
                 console.log('error cargando imagen - cancelando inclusion');
               }
             }
-
           }
         },
-
         theme: 'plain',
-        columnStyles: { 1: { halign: 'right' } },
+        showFoot: 'never',
+        margin: { top: marginTop },
         headStyles: { halign: 'center', textColor: 255, fillColor: 255 },
-        head: [['Datos de la orden', 'Empresa info']],
+        head: [['', '', 'logo section image']],
         body: [
-          [`Fecha: ${order.admissionDateFront}`, `${enterprise.enterprisename}`],
-          [`Orden Nro: ${order.id}`, `${enterprise.location}`],
-          [`Cliente: ${order.clientname}`, `Cel: ${enterprise.cellphone}`],
-          ['', `Tel: ${enterprise.phone}`],
+          [
+            {
+              content: '', styles: { cellWidth: 100, halign: 'center', textColor: 100 }
+            },
+            {
+              content: enterprise.enterprisename + ' Informaticas', styles: { halign: 'center', textColor: 100, fontSize: 16 }
+            },
+            {
+              content: '', styles: { cellWidth: 100, halign: 'right', textColor: 100 }
+            },
+          ],
+          [
+            '',
+            {
+              content: enterprise.location, styles: { halign: 'center', textColor: 100 }
+            },
+            ''
+          ],
+          [
+            {
+              content: `Orden de servicio: ${order.id}`, styles: { textColor: 100 }
+            },
+            {
+              content: enterprise.phone, styles: { halign: 'center', textColor: 100 }
+            },
+            ''
+          ],
+          [
+            {
+              content: `Fecha: ${order.admissionDateFront}`, styles: { textColor: 100 }
+            },
+            {
+              content: enterprise.email, styles: { halign: 'center', textColor: 100 }
+            },
+            ''
+          ],
         ],
       })
 
       doc.autoTable({
+        headStyles: { halign: 'center' },
+        showHead: 'never',
+        showFoot: 'never',
         theme: 'grid',
-        headStyles: { halign: 'center', textColor: 100, fillColor: 200 },
-        bodyStyles: { halign: 'center' },
-        head: [['Articulo', 'Marca', 'Modelo']],
+        head: [['', '']],
         body: [
-          [order.article, order.brand, order.model],
+          [`Nombre del cliente: ${order.clientname}`, `Telefono: ${order.clientphone}`],
+          [`Articulo: ${order.article}`, `Modelo: ${order.model}`],
+          [`Marca: ${order.brand}`, `Serie: No tiene`],
         ],
       })
 
+
       doc.autoTable({
+        headStyles: { halign: 'center' },
+        showHead: 'never',
         theme: 'grid',
-        headStyles: { halign: 'center', textColor: 100, fillColor: 200 },
-        bodyStyles: { halign: 'center' },
-        head: [['Falla Reportada']],
+        head: [['', '']],
         body: [
-          [order.reportedfailure],
+          [
+            {
+              content: 'Problema Reportado: ', styles: { cellWidth: 50, textColor: 100 }
+            },
+            order.reportedfailure],
+          [
+            {
+              content: 'Notas: ', styles: { cellWidth: 50, textColor: 100 }
+            },
+            order.observations],
         ],
       })
+
 
       doc.autoTable({
         theme: 'plain',
+        margin: { top: -15, bottom: -30 },
         columnStyles: { 1: { halign: 'right' } },
         bodyStyles: { halign: 'right', textColor: 100, fillColor: 255 },
         body: [
@@ -77,13 +123,14 @@ export default class InputPdf extends Styles {
           [{
             content: enterprise.firstmessage == undefined
               ? ''
-              : enterprise.secondmessage + '', styles: { halign: 'center', fillColor: 200, textColor: 100 }
+              : enterprise.secondmessage + '', styles: { halign: 'center', fillColor: 240, textColor: 100 }
           }],
-          [''], 
-          ['']
         ]
       })
+
+      marginTop = -40;
     }
+
 
     doc.save('Ingreso');
   }
